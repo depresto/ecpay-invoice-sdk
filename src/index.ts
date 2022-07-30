7e79e96fde2de1dbd3c102ef8f32d4faf843d450
Wayne Huang <wayne123tw@gmail.com>
2022-07-30 15:30:02 +0800

    [BUGFIX] add export

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import EcpayInvoiceClient, {
+  IssueInvoiceResult,
+  RevokeInvoiceResult,
+  InvoiceB2CProps,
+} from "./ecpay-invoice.client";
+
+export { IssueInvoiceResult, RevokeInvoiceResult, InvoiceB2CProps };
+export default EcpayInvoiceClient;
